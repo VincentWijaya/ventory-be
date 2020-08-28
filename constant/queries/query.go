719eefdeff42885b993947a39543df9b0af4f3f0
Vincent-Work <work@MacBook-Pro-Admin.local>
2020-08-28 09:24:41 +0700

    grouping query

--- constant/queries/query.go
+++ constant/queries/query.go
@@ -1,5 +1,6 @@
 package queries
 
+// User
 const (
 	FindUserByUsernameOrEmail = `SELECT u.id, u.username, u.email, u.status, r.role, u.password FROM users as u
 															 JOIN u_roles r ON u.role_id = r.id
@@ -9,15 +10,20 @@ const (
 	InsertUser = "INSERT INTO `users` (`username`, `email`, `password`, `role_id`) VALUES (?, ?, ?, ?)"
 )
 
+// Item Category
 const (
-	GetItemCategory = "SELECT id, name FROM item_category WHERE id>=? AND is_deleted != 1 LIMIT ?"
-	GetItem         = `SELECT i.id, ic.name as category_name, i.name as item_name, i.name, i.buy_price, i.sell_price,
+	GetItemCategory   = "SELECT id, name FROM item_category WHERE id>=? AND is_deleted != 1 LIMIT ?"
+	CountItemCategory = "SELECT COUNT(id) as total FROM item_category"
+)
+
+// Item
+const (
+	GetItem = `SELECT i.id, ic.name as category_name, i.name as item_name, i.name, i.buy_price, i.sell_price,
 	   								 i.stock, i.notes, i.created_at, i.updated_at
 										 FROM items as i
 										 JOIN item_category ic ON i.id = ic.id
 										 WHERE i.id >= ? AND i.is_deleted != 1 LIMIT ?`
 	CountItem            = "SELECT COUNT(id) as total FROM items"
-	CountItemCategory    = "SELECT COUNT(id) as total FROM item_category"
 	FindItemByCategoryID = `SELECT id, name, category_id, buy_price, sell_price,
 													stock, created_at, updated_at, notes FROM items
 													WHERE category_id=?`
